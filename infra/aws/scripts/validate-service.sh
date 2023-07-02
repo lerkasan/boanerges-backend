d278221872c47ef61dff4bff9ef23fd97db426c1
Valeriia Lapytska <lerkasan@gmail.com>
2023-07-02 04:22:15 -0400

    minor fix of validation url

--- infra/aws/scripts/validate-service.sh
+++ infra/aws/scripts/validate-service.sh
@@ -4,11 +4,13 @@ set -x
 NUMBER_OF_ATTEMPTS=10
 SLEEP_TIME=3
 
+URL=http://lerkasan.net
+
 # Ensure Tomcat is running by making an HTTPS GET request to the default page.
 # Don't try and verify the certificate; use the --insecure flag.
 for i in `seq 1 $NUMBER_OF_ATTEMPTS`;
 do
-  HTTP_CODE=`curl --insecure --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost`
+  HTTP_CODE=`curl --insecure --write-out '%{http_code}' -o /dev/null -m 10 -q -s $URL`
   if [ "$HTTP_CODE" == "200" ]; then
     echo "app server is running."
     exit 0
